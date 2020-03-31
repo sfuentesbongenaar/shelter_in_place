f823149f484dd693d99c5bda124f31b5121b7c9c
Fuentes <E.Fuentes.Bongenaar@protonmail.com>
2020-03-31 12:30:28 -0600

    Navigate through the questions after social distancing with dotted navigation in the bottom bar.

--- lib/pages/questions/activities.dart
+++ lib/pages/questions/activities.dart
@@ -1,15 +1,9 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shelter_in_place/models/day_model.dart';
 import 'package:shelter_in_place/pages/localization/localizations.dart';
-import 'package:shelter_in_place/pages/questions/question_bottom_bar.dart';
 import 'package:shelter_in_place/pages/questions/shared_const.dart';
-import 'package:shelter_in_place/pages/util/colors.dart';
 import 'package:shelter_in_place/pages/util/round_checkbox.dart';
-
-import 'my_continue_button.dart';
 
 class Activities extends StatefulWidget {
   @override
@@ -21,8 +15,6 @@ class _ActivitiesState extends State<Activities> {
 
   @override
   Widget build(BuildContext context) {
-    final dayModel = Provider.of<Day>(context);
-
     List<String> activitities = Constants().activitities;
     activitities
         .forEach((activity) => answers.putIfAbsent(activity, () => false));
@@ -32,7 +24,6 @@ class _ActivitiesState extends State<Activities> {
           padding: const EdgeInsets.all(15.0),
           child: LabeledCheckbox(
             label: AppLocalizations.of(context).translate(keyName),
-            
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             value: answers[keyName],
             onChanged: (bool newValue) {
@@ -43,53 +34,34 @@ class _ActivitiesState extends State<Activities> {
           ));
     }).toList();
 
-    CustomContinueButton continueButton = CustomContinueButton(
-      onPressed: () {
-        HashMap chosenActivities = answers;
-        // Filter out all not chosen answers
-        chosenActivities.removeWhere((k, v) => !v);
-        // Save the answers of user in Day model
-        dayModel.activities = chosenActivities.keys.toList();
-        Navigator.pushNamed(context, 'third-question');
-      },
-    );
-
-    return SafeArea(
-        top: true,
-        bottom: false,
-        child: Scaffold(
-            bottomNavigationBar: QuestionBottomBar(
-              continueButton: continueButton,
-            ),
-            body: Center(
-              child: Column(children: <Widget>[
-                SizedBox(height: 20.0),
-                Padding(
-                    padding: const EdgeInsets.fromLTRB(50, 30, 50, 30),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                            AppLocalizations.of(context)
-                                .translate('question activities'),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            )),
-                        SizedBox(height: 20.0),
-                        Text(
-                            AppLocalizations.of(context).translate('check all'),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w200,
-                              color: Colors.black,
-                            ))
-                      ],
+    return Center(
+      child: Column(children: <Widget>[
+        SizedBox(height: 20.0),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(50, 30, 50, 30),
+            child: Column(
+              children: <Widget>[
+                Text(
+                    AppLocalizations.of(context)
+                        .translate('question activities'),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     )),
-                Expanded(child: ListView(children: activitiesBoxes))
-              ]),
-            )));
+                SizedBox(height: 20.0),
+                Text(AppLocalizations.of(context).translate('check all'),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.black,
+                    ))
+              ],
+            )),
+        Expanded(child: ListView(children: activitiesBoxes))
+      ]),
+    );
   }
 }
